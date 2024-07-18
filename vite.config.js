e1b2e087c25d904e3520a4a5b11916d36ce45265
Pankajladwal26 <pankajladwal026@gmail.com>
2024-07-18 22:55:50 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "React Chatbot",
+  base: "/React Chatbot/",
   plugins: [react()],
 })
